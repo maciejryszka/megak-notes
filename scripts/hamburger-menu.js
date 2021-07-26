74ba25a21774eeece6db2fb45e9bc8e5706d9674
maciejryszka <maciejryszka@gmail.com>
2021-07-26 18:49:13 +0200

    add hamburger menu hiding

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -2,9 +2,15 @@ const hamburgerMenuButton = document.querySelector(
   ".header-navigation__button--menu"
 );
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mainElement = document.querySelector(".main");
 
 function showHamburgerMenu() {
   hamburgerMenu.classList.toggle("hamburger-menu--active");
 }
 
+function hideHamburgerMenu() {
+  hamburgerMenu.classList.remove("hamburger-menu--active");
+}
+
 hamburgerMenuButton.addEventListener("click", showHamburgerMenu);
+mainElement.addEventListener("click", hideHamburgerMenu);
